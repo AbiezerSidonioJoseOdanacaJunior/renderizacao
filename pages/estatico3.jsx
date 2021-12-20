040737ca569f01f522a5f587de8bc3db75f09d11
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-20 15:50:39 -0300

    Componente statico com Revalidate

---
+++ pages/estatico3.jsx
@@ -0,0 +1,18 @@
+export function getStaticProps() {
+    return {
+        revalidate: 7,
+        props: {
+            numero: Math.random()
+        }
+    }
+}
+
+
+export default function estatico3(props) {
+    return(
+        <div>
+            <h1>Eatatico#03</h1>
+            <h2>{props.numero}</h2>
+        </div>
+    )
+}
