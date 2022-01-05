5bfce29ac79af3f4470b90515a61a184af0cd490
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-05 14:59:55 -0300

    create dinamic 2

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -1,12 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+function numeroAleatorio(min = 1, max = 10000) {
+  return parseInt(Math.random() * (max - min)) + min;
+}
 
 export default function handler(req, res) {
   res.status(200).json([
-    {id: 1, nome: "Caneta", preco: "4.50"},
-    {id: 2, nome: "Lapis", preco: "3.14"},
-    {id: 3, nome: "Borracha", preco: "2.16"},
-    {id: 4, nome: "Caderno", preco: "10.99"},
-    {id: 5, nome: "Regua", preco: "5.59"},
-    {id: 6, nome: "Compasso", preco: "15.29"}
-  ])
+    { id: numeroAleatorio(), nome: "Caneta", preco: "4.50" },
+    { id: numeroAleatorio(), nome: "Lapis", preco: "3.14" },
+    { id: numeroAleatorio(), nome: "Borracha", preco: "2.16" },
+    { id: numeroAleatorio(), nome: "Caderno", preco: "10.99" },
+    { id: numeroAleatorio(), nome: "Regua", preco: "5.59" },
+    { id: numeroAleatorio(), nome: "Compasso", preco: "15.29" },
+  ]);
 }
