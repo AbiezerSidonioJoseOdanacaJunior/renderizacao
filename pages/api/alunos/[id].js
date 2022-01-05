65779f97116d458c77c8a53eb17b36ce56b7fb5d
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-05 16:41:11 -0300

    Melhorando getStatusPaths

--- pages/api/alunos/[id].js
+++ pages/api/alunos/[id].js
@@ -1,26 +1,8 @@
-export function getStaticPaths() {
-  return {
-    fallback: false, //204
-    paths: [
-      { paths: { id: "105" } },
-      { paths: { id: "100" } },
-      { paths: { id: "2050" } },
-      { paths: { id: "110" } },
-    ],
-  };
-}
-
-export function getStaticProps() {
-  return {
-    props: {},
-  };
-}
-
 export default function handler(req, res) {
   const id = +req.query.id;
   res.status(200).json({
     id,
-    nome: `Bibi Junior ${id}`,
-    email: `bibijnr${id}@bibi.com`,
+    nome: `Bibi Junir ${id}`,
+    email: `bibijnr {id}@gmail.com`,
   });
 }
